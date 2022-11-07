c57cd34cbd5758f3724895d122ba31d1c9ce8926
Sue <sue4100035045@gmail.com>
2022-11-07 13:25:01 +0800

    feat: virtual list

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,10 +1,17 @@
-import { useCallback } from 'react';
+import { useCallback, useMemo } from 'react';
 import styled from 'styled-components';
 
+const BoxContainer = styled.div`
+  position: absolute;
+  padding: 10px 0;
+  width: 100%;
+  height: 245px;
+`;
+
 const ItemBox = styled.div`
-  margin: 20px 0;
   padding: 10px;
-  box-shadow: 0 0 5px 1px rgba(0, 0, 0, 0.1);
+  height: 100%;
+  border: 1px solid #eee;
   border-radius: 5px;
   line-height: 1.5;
 
@@ -17,7 +24,7 @@ const ItemBox = styled.div`
   }
 `;
 
-export default function ItemCard({ item }) {
+export default function ItemCard({ item, index }) {
   const timeFormat = useCallback(time => {
     const currTime = new Date(time);
     const year = currTime.getFullYear().toString().padStart(4, '0');
@@ -27,30 +34,36 @@ export default function ItemCard({ item }) {
     return `${year}/${month}/${date}`;
   }, []);
 
+  const itemTop = useMemo(() => {
+    return `${245 * index}px`;
+  }, [index]);
+
   return (
-    <ItemBox>
-      <p>
-        <span>Full Name:</span> {item.full_name}
-      </p>
-      <p>
-        <span>Forks:</span> {item.forks}
-      </p>
-      <p>
-        <span>Create Date:</span> {timeFormat(item.created_at)}
-      </p>
-      <p>
-        <span>Update Date:</span> {timeFormat(item.updated_at)}
-      </p>
-      <p>
-        <span>Pushed Date:</span> {timeFormat(item.pushed_at)}
-      </p>
-      <p>
-        <span>Language:</span> {item.language}
-      </p>
-      <p className="description">{item.description}</p>
-      <a href={item.html_url} target="_blank" rel="noreferrer noopenner">
-        LINK
-      </a>
-    </ItemBox>
+    <BoxContainer style={{ top: itemTop }}>
+      <ItemBox>
+        <p>
+          <span>Full Name {index}:</span> {item.full_name}
+        </p>
+        <p>
+          <span>Forks:</span> {item.forks}
+        </p>
+        <p>
+          <span>Create Date:</span> {timeFormat(item.created_at)}
+        </p>
+        <p>
+          <span>Update Date:</span> {timeFormat(item.updated_at)}
+        </p>
+        <p>
+          <span>Pushed Date:</span> {timeFormat(item.pushed_at)}
+        </p>
+        <p>
+          <span>Language:</span> {item.language}
+        </p>
+        <p className="description">{item.description}</p>
+        <a href={item.html_url} target="_blank" rel="noreferrer noopenner">
+          LINK
+        </a>
+      </ItemBox>
+    </BoxContainer>
   );
 }
